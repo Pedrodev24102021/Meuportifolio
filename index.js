8fd3ce1ec520ec662476735187171954d4a91877
Pedrodev24102021 <102605096+Pedrodev24102021@users.noreply.github.com>
2023-01-01 12:30:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+////// digitação/////////
+const saida = document.querySelector('#saida');
+function digitacao(texto, contador) {
+  if (contador < texto.length) {
+    setTimeout(() => {
+      saida.textContent += texto.charAt(contador);
+      contador++;
+      digitacao(texto, contador);
+    }, 50);
+  }
+}
+digitacao(
+  'Ola meu nome é Pedro Henrique Curso Analise e Desenvolvimento de Sistemas,Em paralelo invisto em cursos para Front-End e UI/UX Designer!',
+  0,
+);
+////////modal////////
+const ativarmodal = document.querySelector('.link-tech');
+const desativarmodal = document.querySelector('.fecharmodal');
+
+function active() {
+  document.querySelector('.conteudotech').classList.add('active');
+}
+function desativar() {
+  document.querySelector('.conteudotech').classList.remove('active');
+}
+desativarmodal.addEventListener('click', desativar);
+ativarmodal.addEventListener('click', active);
+/////menuhamburguer/////
+
+const btnmobile = document.querySelector('#btn-mobile');
+function activemenu() {
+  document.querySelector('#nav').classList.toggle('activemenu');
+}
+
+btnmobile.addEventListener('click', activemenu);
